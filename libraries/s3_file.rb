af8dd89fee7bf52cfee1779e5a0d8867e13d065f
Brandon Adams <brandon.adams@me.com>
2013-05-11 12:44:08 -0700

    Only download when local file differs from S3
    Added code that will compare the ETag header in S3, which is an md5sum,
    with the md5sum of the local file on disk.  If they match, then no
    download will be performed.

--- libraries/s3_file.rb
+++ libraries/s3_file.rb
@@ -4,7 +4,22 @@ require 'openssl'
 require 'base64'
 
 module S3File
-  def get_from_s3(bucket,path,aws_access_key_id,aws_secret_access_key)
+  def get_md5_from_s3(bucket,path,aws_access_key_id,aws_secret_access_key)
+    now = Time.now().utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
+    string_to_sign = "HEAD\n\n\n%s\n/%s%s" % [now,bucket,path]
+
+    digest = digest = OpenSSL::Digest::Digest.new('sha1')
+    signed = OpenSSL::HMAC.digest(digest, aws_secret_access_key, string_to_sign)
+    signed_base64 = Base64.encode64(signed)
+
+    auth_string = 'AWS %s:%s' % [aws_access_key_id,signed_base64]
+
+    response = RestClient.head('https://%s.s3.amazonaws.com%s' % [bucket,path], :date => now, :authorization => auth_string)
+
+    return response.headers['ETag']
+  end
+  
+  def get_from_s3(bucket,path,aws_access_key_id,aws_secret_access_key)    
     now = Time.now().utc.strftime('%a, %d %b %Y %H:%M:%S GMT')
     string_to_sign = "GET\n\n\n%s\n/%s%s" % [now,bucket,path]
 
--- providers/default.rb
+++ providers/default.rb
@@ -1,15 +1,31 @@
 include S3File
 
+require 'digest/md5'
+
 action :create do
   new_resource = @new_resource
+  download = true
+  
+  if ::File.exists? new_resource.path
+    s3_md5 = get_md5_from_s3(new_resource.bucket, new_resource.remote_path, new_resource.aws_access_key_id, new_resource.aws_secret_access_key)
+  
+    current_md5 = Digest::MD5.hexdigest(::File.read(new_resource.path))
+  
+    if current_md5 == s3_md5 then
+      Chef::Log.debug 'Skipping download, md5sum of local file matches file in S3.'
+      download = false
+    end
+  end
+  
+  if download
+    body = get_from_s3(new_resource.bucket, new_resource.remote_path, new_resource.aws_access_key_id, new_resource.aws_secret_access_key).body
 
-  body = get_from_s3(new_resource.bucket, new_resource.remote_path, new_resource.aws_access_key_id, new_resource.aws_secret_access_key).body
-
-  file new_resource.path do
-    owner new_resource.owner if new_resource.owner
-    group new_resource.group if new_resource.group
-    mode new_resource.mode if new_resource.mode
-    action :create
-    content body
+    file new_resource.path do
+      owner new_resource.owner if new_resource.owner
+      group new_resource.group if new_resource.group
+      mode new_resource.mode if new_resource.mode
+      action :create
+      content body
+    end
   end
 end
