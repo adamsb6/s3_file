fbad716ccbc95897b064cda4e09941f56b2b2468
Joseph Lawson <joe@joekiller.com>
2015-03-20 17:16:49 -0400

    s3_file version 2.5.3

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "brandon.adams@me.com"
 license          "MIT"
 description      "Installs/Configures s3_file LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "2.5.2"
+version          "2.5.3"
