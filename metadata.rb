8a97160297152c79fe529db7e9780414b0b759c0
Joseph Lawson <joe@joekiller.com>
2016-02-01 13:12:43 -0500

    version 2.7

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "brandon.adams@me.com"
 license          "MIT"
 description      "Installs/Configures s3_file LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.6.6"
+version          "2.7"
