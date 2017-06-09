5a4f15392968061b572f572cd3a36d287341735b
Joe Khoobyar <joe@khoobyar.name>
2017-06-09 15:19:18 -0400

    fix typo

--- providers/default.rb
+++ providers/default.rb
@@ -24,7 +24,7 @@ action :create do
   aws_secret_access_key = new_resource.aws_secret_access_key
   token = new_resource.token
   decryption_key = new_resource.decryption_key
-  region = new_resource.region
+  region = new_resource.aws_region
 
   # if credentials not set, try instance profile
   if aws_access_key_id.nil? && aws_secret_access_key.nil? && token.nil?
