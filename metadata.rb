c444c997406909cb2d881d5e1448f7f533477c1e
Stefan Wessels Beljaars <swesselsbeljaars@schubergphilis.com>
2017-08-29 11:17:25 +0200

    Updated metadata to comply with FC

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,13 @@ maintainer_email "brandon.adams@me.com"
 license          "MIT"
 description      "Installs/Configures s3_file LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.9.0.pre"
+version          "2.8.1"
+
+source_url 'https://github.com/adamsb6/s3_file' if responds_to(:source_url)
+issues_url 'https://github.com/adamsb6/s3_file/issues' if responds_to(:issues_url)
+
+chef_version '>= 12.6' if responds_to(:chef_version)
+
+supports 'centos'
+supports 'redhat'
+supports 'windows'
