99e51285756c6a778e67fef4b10772775a2d0bbf
hiteshri <hiteshri.ramteke@reancloud.com>
2017-09-14 12:01:28 +0530

    Conflicts in attributes file is resolved

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 default['s3_file']['mime-types']['version'] = '3.1'
 default['s3_file']['rest-client']['version'] = '2.0.2'
+
+# Keep a catalog of each downloaded file's etag and md5 at time of download.
+default['s3_file']['use_catalog'] = true
