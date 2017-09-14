9ef02c04bdb35f1058161dc3a7ac4c4258c21acf
Joseph Lawson <joe@joekiller.com>
2017-09-14 09:20:38 -0400

    s3_file v2.8.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "brandon.adams@me.com"
 license          "MIT"
 description      "Installs/Configures s3_file LWRP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.8.4"
+version          "2.8.5"
 
 source_url 'https://github.com/adamsb6/s3_file' if responds_to(:source_url)
 issues_url 'https://github.com/adamsb6/s3_file/issues' if responds_to(:issues_url)
