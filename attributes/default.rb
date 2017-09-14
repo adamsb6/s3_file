e4930244a882e2dcccda67ec7ccc30165495b7ba
hiteshri <hiteshri.ramteke@reancloud.com>
2017-09-14 12:26:27 +0530

    Changing the versions of 'mime-types' and 'rest-client' attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['s3_file']['mime-types']['version'] = '2.6.2'
-default['s3_file']['rest-client']['version'] = '1.7.3'
+default['s3_file']['mime-types']['version'] = '3.1'
+default['s3_file']['rest-client']['version'] = '2.0.2'
 
 # Keep a catalog of each downloaded file's etag and md5 at time of download.
 default['s3_file']['use_catalog'] = true
